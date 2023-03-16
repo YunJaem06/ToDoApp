adf2247f7e589d11fc7ae598e772c99106a01202
yunjaem06 <yunjae0616@gmail.com>
2023-03-16 18:26:39 +0900

    add 디자인 구현 화면이동

--- app/src/main/java/com/example/todolist/view/add/AddFragment.kt
+++ app/src/main/java/com/example/todolist/view/add/AddFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.todolist.BaseFragment
 import com.example.todolist.R
 import com.example.todolist.databinding.FragmentAddBinding
 
 class AddFragment : BaseFragment<FragmentAddBinding>(R.layout.fragment_add) {
     override fun init() {
+
+        binding.ivAddBack.setOnClickListener {
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }
 
     }
 
