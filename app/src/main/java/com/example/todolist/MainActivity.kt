95566384afe2c1a3c7441fbe4c396cc08e999584
yunjaem06 <yunjae0616@gmail.com>
2023-03-20 18:57:01 +0900

    toolbar 커스텀 완료 적용까지

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.todolist.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
--- app/src/main/java/com/example/todolist/view/list/ListFragment.kt
+++ app/src/main/java/com/example/todolist/view/list/ListFragment.kt
@@ -1,11 +1,8 @@
 package com.example.todolist.view.list
 
-import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuProvider
 import androidx.navigation.fragment.findNavController
 import com.example.todolist.BaseFragment
 import com.example.todolist.R
@@ -21,5 +18,21 @@ class ListFragment : BaseFragment<FragmentListBinding>(R.layout.fragment_list) {
         binding.listLayout.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
+
+        binding.toolbarList.addMenuProvider(object  : MenuProvider{
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.list_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when(menuItem.itemId){
+                    R.id.menu_delete_all -> {
+                        Log.i("myTag", "모두삭제 선택")
+                    }
+                }
+                return true
+            }
+
+        })
     }
 }
