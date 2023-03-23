6fdd790429031eb660b044dc02efb36c8b2e8fa3
yunjaem06 <yunjae0616@gmail.com>
2023-03-23 18:06:10 +0900

    Room Database

---
+++ app/src/main/java/com/example/todolist/data/ToDoDatabase.kt
@@ -0,0 +1,8 @@
+package com.example.todolist.data
+
+import androidx.room.Database
+import com.example.todolist.data.model.ToDoData
+
+@Database(entities = [ToDoData::class], version = 1)
+class ToDoDatabase {
+}
