40dbfaaa5c346e8aaac0569dd5be43f876b790f6
yunjaem06 <yunjae0616@gmail.com>
2023-03-24 12:46:53 +0900

    main icon 등록

--- app/src/main/java/com/example/todolist/data/ToDoDatabase.kt
+++ app/src/main/java/com/example/todolist/data/ToDoDatabase.kt
@@ -3,6 +3,6 @@ package com.example.todolist.data
 import androidx.room.Database
 import com.example.todolist.data.model.ToDoData
 
-@Database(entities = [ToDoData::class], version = 1)
-class ToDoDatabase {
-}
+//@Database(entities = [ToDoData::class], version = 1)
+//class ToDoDatabase {
+//}
