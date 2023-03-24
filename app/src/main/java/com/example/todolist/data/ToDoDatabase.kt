2f68fd5bbcbd76a7e8824cc995a7c1a38e1eeb30
yunjaem06 <yunjae0616@gmail.com>
2023-03-24 16:34:53 +0900

    Room database

--- app/src/main/java/com/example/todolist/data/ToDoDatabase.kt
+++ app/src/main/java/com/example/todolist/data/ToDoDatabase.kt
@@ -1,8 +1,36 @@
 package com.example.todolist.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.example.todolist.data.model.ToDoData
 
-//@Database(entities = [ToDoData::class], version = 1)
-//class ToDoDatabase {
-//}
+@Database(entities = [ToDoData::class], version = 1)
+abstract class ToDoDatabase : RoomDatabase() {
+
+//    abstract fun toDoDao() : ToDoDao
+    abstract val toDoDao : ToDoDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: ToDoDatabase? = null
+
+        fun getInstance(context: Context) : ToDoDatabase {
+
+            synchronized(this){
+                var instance = INSTANCE
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        ToDoDatabase::class.java,
+                        "todo_database"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
