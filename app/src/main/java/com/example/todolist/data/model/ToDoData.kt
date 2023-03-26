e69084838e7cdeed483cd102d2abc46c88400c36
yunjaem06 <yunjae0616@gmail.com>
2023-03-26 15:12:47 +0900

    [SQLITE_ERROR] SQL error or missing database 오류 해결

--- app/src/main/java/com/example/todolist/data/model/ToDoData.kt
+++ app/src/main/java/com/example/todolist/data/model/ToDoData.kt
@@ -8,15 +8,11 @@ import androidx.room.PrimaryKey
 data class ToDoData(
 
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "todo_id")
     var id: Int,
 
-    @ColumnInfo(name = "todo_title")
     var title: String,
 
-    @ColumnInfo(name = "todo_priority")
     var priority: Priority,
 
-    @ColumnInfo(name = "todo_description")
     var description: String
 )
